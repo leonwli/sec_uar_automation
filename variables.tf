84f8debfa91b14f548e9197e81cb54706d972db8
Leon Li <leon.li@wesfarmershealth.com.au>
2025-04-09 12:06:25 +1000

    .

--- variables.tf
+++ variables.tf
@@ -24,30 +24,28 @@ variable "automation_account" {
 }
 
 variable "resource_group" {
-  type        = string
+  type    = string
   default = "secautomation-rg"
 }
 
 variable "automation_account" {
-  type        = string
+  type    = string
   default = "secautomation"
 }
 
 variable "runbook_name" {
-  description = "Name of the runbook created" 
+  description = "Name of the runbook created"
   type        = string
 }
 
 variable "runbook_script_path" {
   description = "file path of the Powershell script"
   type        = string
-  default = "./runbook.ps1"
 }
 
 variable "pfx_path" {
   description = "file path of the certificate private key"
   type        = string
-  default = "./cert.pfx"
 }
 
 variable "pfx_password" {
